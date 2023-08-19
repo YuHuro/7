d24cf3408e26c4d84ea1be6daca50f2f9e24a9d9
YuHuro <ayubovmagomed999@gmail.com>
2023-08-19 18:36:56 +0300

    Описание изменений

--- js.js
+++ js.js
@@ -1,7 +1,27 @@
-const evenNumbers = [];
-
-for (let i = 2; i <= 1000; i += 2) {
-  evenNumbers[evenNumbers.length] = i;
+let a = [];
+for(let i = 0; i <= 1000; i++){
+    if(i % 2 === 0){
+        a.push(i)
+    }
 }
 
-console.log(evenNumbers);
+let changeA = []
+
+for(let i = 0; i < a.length; i++){
+    if(a[i] % 3 === 0){
+        changeA.push(a[i])
+    }
+};
+
+const mapA = changeA.map(item => item ** 2);
+
+let index = 0
+
+for(i = 0; i < mapA.length; i++){
+    if(mapA[i] === 324){
+        index = i
+        break;
+    }
+}
+
+console.log(index);
