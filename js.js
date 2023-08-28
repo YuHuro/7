d37a02d01847ae7f95de58a691d3add0af24a980
YuHuro <ayubovmagomed999@gmail.com>
2023-08-28 18:09:18 +0300

    hello

--- js.js
+++ js.js
@@ -1,27 +1,29 @@
-let a = [];
-for(let i = 0; i <= 1000; i++){
-    if(i % 2 === 0){
-        a.push(i)
+const input = document.querySelector('#mainInp')
+const button = document.querySelector('#mainBtn')
+const block = document.querySelector('#mainBlock')
+
+function output(){
+    block.innerHTML += '<h1>' + input.value + '</h1>'
+}
+
+button.addEventListener('click', output);
+
+const num1 = document.querySelector('#num1')
+const num2 = document.querySelector('#num2')
+const equal = document.querySelector('#equals')
+const block2 = document.querySelector('#secondMainBlock')
+
+function calc(){
+    let number1 = parseInt(num1.value)
+    let number2 = parseInt(num2.value)
+    let calc = number1 + number2
+    for(let i=0; i<1; i++){
+        if(isNaN(number1) || isNaN(number2)){
+            block2.innerHTML += '<h1>error</h1>'
+        }else{
+            block2.innerHTML += '<h1>' + calc + '</h1>'
+        }
     }
 }
 
-let changeA = []
-
-for(let i = 0; i < a.length; i++){
-    if(a[i] % 3 === 0){
-        changeA.push(a[i])
-    }
-};
-
-const mapA = changeA.map(item => item ** 2);
-
-let index = 0
-
-for(i = 0; i < mapA.length; i++){
-    if(mapA[i] === 324){
-        index = i
-        break;
-    }
-}
-
-console.log(index);
+equal.addEventListener('click', calc)
